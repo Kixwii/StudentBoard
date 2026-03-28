2e1e594baf54c027b38045e2cb52a56d500c8fc6
magvtv <magutujr@gmail.com>
2026-03-28 21:28:08 +0300

    chore: comment out phoenix controller

--- school_portal_api/lib/school_portal_api_web/auth_error_handler.ex
+++ school_portal_api/lib/school_portal_api_web/auth_error_handler.ex
@@ -1,6 +1,6 @@
 defmodule SchoolPortalApiWeb.AuthErrorHandler do
   import Plug.Conn
-  import Phoenix.Controller
+#  import Phoenix.Controller
 
   def auth_error(conn, {type, _reason}, _opts) do
     body = Jason.encode!(%{error: to_string(type)})
